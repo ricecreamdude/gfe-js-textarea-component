bdf850e7cac9309deb537e43ef99bb0d2f2bfb73
Joshua Ho <ho.joshua4@gmail.com>
2024-07-10 22:52:34 -0700

    Adding number counting functionality

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 // Write custom JavaScript here.
 // You may ignore this file and delete if if JavaScript is not required for your challenge.
+
+const textAreaEl = document.getElementById("textarea")
+const charactersUsedEl = document.getElementById("characters-used");
+
+textAreaEl.addEventListener("keyup", () => {
+    charactersUsedEl.innerHTML = textAreaEl.value.length.toString() + "/500";
+})
